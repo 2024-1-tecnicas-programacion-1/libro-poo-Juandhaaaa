173770f3b7be667ecc6a8af8febb528b66c68812
JD Valeroso <juan.valero.9c@gmail.com>
2024-04-25 10:19:47 -0500

    Taller 2
    Se hace la entrega del programa de libro, para el taller 2, con sus debidos nombres a la complejidad del mismo.

--- src/main/java/com/mycompany/libropoo/Libro.java
+++ src/main/java/com/mycompany/libropoo/Libro.java
@@ -1,5 +1,29 @@
 package com.mycompany.libropoo;
 
 public class Libro {
+    private int anoPublicacion;
+    private String titulo;
+    private String autor;
+    private String genero;
+    private boolean leido;
     
+    public Libro(int annioPublicacion, String titulo,String autor, String genero, boolean leido){
+        this.anoPublicacion = annioPublicacion;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.genero = genero;
+        this.leido = leido;
+    }
+    public String mostrarInformación(){
+        return ("Título: " + titulo + ", Autor: " + autor + ", Año de publicación: " + anoPublicacion + ", Género: " + genero);
+    }
+    public void marcarLeido(){
+        leido = true;
+    }
+    public boolean esAntiguo(){
+        return (2024 - anoPublicacion>50?true:false);
+    }
+    public boolean getLeido(){
+        return this.leido;
+    }
 }
